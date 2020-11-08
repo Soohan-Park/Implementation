44c387e5ef5df91bd431c5f753d358c86eb9fac6
Soohan Park <725psh@gmail.com>
2020-11-08 17:11:19 +0900

    1_Stack & Queue (1)

---
+++ 1_Stack & Queue/Stack & Queue.py
@@ -0,0 +1,40 @@
+# 구현 - Stack & Queue
+
+class Stack:
+    _stack = []
+
+    def add(self, data):
+        self._stack.append(data)
+
+    def pop(self):
+        return self._stack.pop()
+
+
+class Queue:
+    _queue = []
+
+    def add(self, data):
+        self._queue.append(data)
+
+    def pop(self):
+        return self._queue.pop(0)
+
+
+def test():
+    # Stack
+    stack = Stack()
+    stack.add(1)
+    stack.add(2)
+    resStack = stack.pop()
+    print('Result(Stack) >> {}'.format(resStack))
+
+    # Queue
+    queue = Queue()
+    queue.add(1)
+    queue.add(2)
+    resQueue = queue.pop()
+    print('Result(Queue) >> {}'.format(resQueue))
+
+
+if __name__ == '__main__':
+    test()
